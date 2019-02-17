2e6aa757e7e69a981ee8b5e52ee7a7ca69eeca14
huhao <826538400@qq.com>
2019-02-17 12:07:05 +0800

    blog update

--- js/src/hitokoto.js
+++ js/src/hitokoto.js
@@ -4,13 +4,14 @@ window.onload=function () {
     update();
     function update() {
         gethi = new XMLHttpRequest();
-        gethi.open("GET","sslapi.hitokoto.cn/?c=a");
+        gethi.open("GET","https://sslapi.hitokoto.cn/?c=a");
+        //这里选择类别，详见官方文档
         gethi.send();
         gethi.onreadystatechange = function () {
             if (gethi.readyState===4 && gethi.status===200) {
                 var Hi = JSON.parse(gethi.responseText);
                 hitokoto.innerHTML = Hi.hitokoto;
-                from.innerHTML = "—— <i>" + Hi.from + "</i>"; 
+                from.innerHTML = "from: <b>" + Hi.from + "</b>"; //可自定义输出格式
             }
         }
     }
