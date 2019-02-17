0bd4df686deded2047099a684ffbe3b11564cb3c
huhao <826538400@qq.com>
2019-02-17 12:19:12 +0800

    blog update

--- js/src/hitokoto.js
+++ js/src/hitokoto.js
@@ -4,15 +4,15 @@ window.onload=function () {
     update();
     function update() {
         gethi = new XMLHttpRequest();
-        gethi.open("GET","https://huhaohitokoto.github.io/1");
+        gethi.open("GET","https://sslapi.hitokoto.cn/");
         //这里选择类别，详见官方文档
         gethi.send();
         gethi.onreadystatechange = function () {
-//            if (gethi.readyState===4 && gethi.status===200) {
+            if (gethi.readyState===4 && gethi.status===200) {
                 var Hi = JSON.parse(gethi.responseText);
                 hitokoto.innerHTML = Hi.hitokoto;
                 from.innerHTML = "—— <i>" + Hi.from + "</i>"; //可自定义输出格式
-//            }
+            }
         }
     }
 }
